a4fddff3d20dbfd1f26ad2fb6fe4bd1d34f9055c
Lalo Becerra <lalobecerra7@gmail.com>
2018-11-02 22:37:36 -0600

    Formulario

--- vistas/v_formulario.php
+++ vistas/v_formulario.php
@@ -1,3 +1,94 @@
-<div>
-	<h4>HOLA</h4>
+
+
+	<br>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-lg-6 col-sm-6">
+				<div class="form-group">
+					<label class="control-label">Nombre de gerente*</label>
+						<input name="nombre" id="nombre" type="text" class="form-control" parsley-required="true" parsley-trigger="change" placeholder="Nombre">
+				</div>
+		</div>
+		<div class="col-lg-6 col-sm-6">
+				<div class="form-group">
+					<label class="control-label">Apellidos de gerente*</label>
+						<input name="apellidos" id="apellidos" type="text" class="form-control" parsley-required="true" parsley-trigger="change" placeholder="Apellidos">
+				</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-6 col-sm-6">
+			<label class="control-label">Correo electrónico*</label>
+			<div class="input-group">	
+			  <div class="input-group-prepend">
+			    <span class="input-group-text" id="basic-addon1">@</span>
+			  </div>
+			  <input type="text" class="form-control" placeholder="Ej. ejemplo@gmail.com" aria-label="Username" aria-describedby="basic-addon1" id="correo" name="correo">
+			</div>
+		</div>
+		<br>
+		<div class="col-lg-6 col-sm-6">
+			<div class="form-group">
+				<label class="control-label">Celular*</label>
+					<input name="telefono" id="telefono" type="text" class="form-control" parsley-required="true" parsley-trigger="change" placeholder="(XXX) XXXX XXX">
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-3 col-sm-6">
+			<div class="form-group">
+				<label class="control-label">Nombre de usuario*</label>
+					<input name="nombreUsuario" id="nombreUsuario" type="text" class="form-control" parsley-required="true" parsley-trigger="change" placeholder="Ej. NombreUsuario123">
+			</div>
+		</div>
+		<div class="col-lg-3 col-sm-6">
+			<div class="form-group">
+				<label class="control-label">Empresa*</label>
+					<input name="nombreEmpresa" id="nombreEmpresa" type="text" class="form-control" parsley-required="true" parsley-trigger="change" placeholder="Ej. Empresas S.A de C.V.">
+			</div>
+		</div>
+		<div class="col-lg-3 col-sm-6">
+			<div class="form-group">
+				<label class="control-label">Telefono*</label>
+					<input name="telefonoEmpresa" id="telefonoEmpresa" type="text" class="form-control" parsley-required="true" parsley-trigger="change" placeholder="(XXX) XXXX XXX">
+			</div>
+		</div>
+		<div class="col-lg-3 col-sm-6">
+			<div class="form-group">
+				<label class="control-label">Domicilio*</label>
+					<input name="domicilioEmpresa" id="domicilioEmpresa" type="text" class="form-control" parsley-required="true" parsley-trigger="change" placeholder="Ej. Calle Centro #25">
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-4 col-sm-6">
+			<label class="control-label">Giro*</label>
+			<select name="giroEmpresa" id="giroEmpresa" class="form-control">
+				<option value="0" disabled selected>Seleccione una opción</option>
+				<option value="farmacia">Farmacia</option>
+				<option value="abarrotes">Abarrotes</option>
+				<option value="ferreterias">Ferreterias</option>
+				<option value="refaccionaria">Refaccionaria</option>
+				<option value="boutique">Boutique</option>
+				<option value="servicios">Servicios</option>
+				<option value="restaurant">Restaurant</option>
+				<option value="otro">Otro</option>
+			</select>
+		</div>
+		<div class="col-lg-4 col-sm-6">
+			<label class="control-label">Paquete*</label>
+			<select name="paquete" id="paquete" class="form-control">
+				<option value="0" disabled selected>Seleccione una opción</option>
+				<option value="Freemium">Freemium</option>
+				<option value="Premium">Premium</option>
+				<option value="Plus">Plus</option>
+			</select>
+		</div>
+		<div class="col-lg-4 col-sm-6">
+			<div class="form-group">
+				<label class="control-label">Captcha*</label>
+					<input name="domicilioEmpresa" id="domicilioEmpresa" type="text" class="form-control" parsley-required="true" parsley-trigger="change" placeholder="Ej. Calle Centro #25">
+			</div>
+		</div>
+	</div>
 </div>
